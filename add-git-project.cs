1a153e3eb783ed81f5b16b5c4fb6dc71b125e4c8
Ruslan <rs@jelastic.com>
2016-07-24 20:38:35 +0200

    first test

---
+++ add-git-project.cs
@@ -0,0 +1,32 @@
+
+//@auth
+
+var params = {
+   envName: "${env.domain}",
+   session: session,
+   type: "git",
+   context: "test",
+   url: "https://github.com/jelastic/HelloWorld.git",
+   branch "master",
+   keyId: 2117,
+   login: "git",
+   autoupdate: true,
+   interval: 1,
+   autoResolveConflict: true,
+   zdt: false
+}
+
+var resp = jelastic.env.vcs.CreateProject(params);
+
+return {
+    result : 0,
+    respone: resp,
+    onAfterReturn : {
+        call : , {
+            procedure: 'log',
+            params: {
+                message: result
+            }
+        }
+    }
+};
