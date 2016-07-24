ad0bc86a11d130446a90ac6e5a94a45d4abb541d
Ruslan Synytsky <siruslan@gmail.com>
2016-07-25 00:00:00 +0200

    Update add-git-project.cs

--- add-git-project.cs
+++ add-git-project.cs
@@ -41,7 +41,7 @@ return {
             {
                 procedure: 'log',
                 params: {
-                    message: params.envName
+                    message: "bb"
                 }
             }
         ]
