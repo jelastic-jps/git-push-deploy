5cf79609e35cff49b9bd963b408d05f0a27eec91
aRefox <a.kyrychuk@gmail.com>
2016-08-10 18:00:33 +0300

    Update manifest.jps + add-git-project.cs

--- add-git-project.cs
+++ add-git-project.cs
@@ -17,18 +17,29 @@ var params = {
    zdt: false
 }
 
+var resp;
 SshKeyResponse = jelastic.users.account.GetSSHKeys(appid, session, true);
 if (SshKeyResponse.result != 0) return SshKeyResponse;
-if (SshKeyResponse.keys != null && SshKeyResponse.keys.length > 0) params.keyId = SshKeyResponse.keys[0].id;
+if (SshKeyResponse.keys != null && SshKeyResponse.keys.length > 0)
+{
+    for (i = 0; i < SshKeyResponse.keys.length; i++) {
+        params.keyId = SshKeyResponse.keys[i].id;
+        resp = jelastic.env.vcs.CreateProject(params.envName, params.session, params.type, params.project, params.url, params.branch, params.keyId, params.login, params.password, params.autoupdate, params.interval, params.autoResolveConflict, params.zdt);
+        if (resp.result == 0)
+        {
+            resp = jelastic.env.vcs.Update(params.envName, params.session, params.project);
+            if (resp.result == 0) break;
+            else
+                jelastic.env.vcs.DeleteProject(params.envName, params.session, params.project);
+        }
+    }  
+    return resp;
+}
 else
 return {
     result: 81,
     keyId: "Private SSH keys was not found"
 };
-
-var resp = jelastic.env.vcs.CreateProject(params.envName, params.session, params.type, params.project, params.url, params.branch, params.keyId, params.login, params.password, params.autoupdate, params.interval, params.autoResolveConflict, params.zdt);
-if (resp.result == 0) resp = jelastic.env.vcs.Update(params.envName, params.session, params.project); 
-return resp;
 
 /*
 return {
