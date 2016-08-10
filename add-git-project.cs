f63433d0ca80faf3b19eb64366d39f2c4cf3ecaa
aRefox <a.kyrychuk@gmail.com>
2016-08-10 10:48:19 +0300

    Add automatic KeyId

--- add-git-project.cs
+++ add-git-project.cs
@@ -1,20 +1,30 @@
- //@req(session, url, keyId, login)
+ //@req(session, appid, url, login, password)
 
 var params = {
+   appId: appid,
    envName: "${env.appid}",
    session: session,
    type: "git",
    project: "ROOT",
    url: url,
    branch: "master",
-   keyId: keyId,
+   keyId: "",
    login: login,
-   password: null,
+   password: password,
    autoupdate: true,
    interval: 1,
    autoResolveConflict: true,
    zdt: false
 }
+
+SshKeyResponse = jelastic.users.account.GetSSHKeys(appid, session, true);
+if (SshKeyResponse.result != 0) return SshKeyResponse;
+if (SshKeyResponse.keys != null && SshKeyResponse.keys.length > 0) return SshKeyResponse.keys[0].id;
+else
+return {
+    result: 81,
+    keyId: "Private SSH keys was not found"
+};
 
 var resp = jelastic.env.vcs.CreateProject(params.envName, params.session, params.type, params.project, params.url, params.branch, params.keyId, params.login, params.password, params.autoupdate, params.interval, params.autoResolveConflict, params.zdt);
 if (resp.result == 0) resp = jelastic.env.vcs.Update(params.envName, params.session, params.project); 
