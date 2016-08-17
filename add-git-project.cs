4fbd1e4fc70d717baa310f10c53eac86bef5e1c2
Ruslan Synytsky <siruslan@gmail.com>
2016-08-17 09:15:12 +0200

    Update add-git-project.cs

--- add-git-project.cs
+++ add-git-project.cs
@@ -29,6 +29,8 @@ if (resp.keys.length == 0) {
 
 //getting the first private key   
 params.keyId = resp.keys[0].id;
+
+//create and update the project 
 resp = jelastic.env.vcs.CreateProject(params.envName, params.session, params.type, params.project, params.url, params.branch, params.keyId, params.login, params.password, params.autoupdate, params.interval, params.autoResolveConflict, params.zdt);
 if (resp.result != 0) return resp;
 resp = jelastic.env.vcs.Update(params.envName, params.session, params.project);
