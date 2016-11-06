fd6b027e6e3f145fec7364e2b0e039a7ba045749
sych74 <sych74@gmail.com>
2016-11-06 22:50:40 +0100

    Update url-hook-handler.cs

--- scripts/url-hook-handler.cs
+++ scripts/url-hook-handler.cs
@@ -1,7 +1,35 @@
-//@req(token)
+//@req(json)
 
-if (token == "${TOKEN}") {
-  return jelastic.env.vcs.Update("${ENV_NAME}", signature, "${PROJECT}");
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Mac;
+
+var hubSignature = window.location.headers["X-Hub-Signature"];
+if (hubSignature == hmacSha1(json, token)) {
+    return jelastic.env.vcs.Update("env-0448488", signature, "ROOT");
 } else {
-  return {"result": 8, "error": "wrong token"}
+    return {
+        "result": 8,
+        "error": "verification X-Hub-Signature error"
+    }
+}
+
+function hmacSha1(value, key) {
+
+    // Get an hmac_sha1 key from the raw key bytes
+    var keyBytes = new java.lang.String(key).getBytes();
+    var signingKey = new SecretKeySpec(keyBytes, "HmacSHA1");
+
+    // Get an hmac_sha1 Mac instance and initialize with the signing key
+    var mac = Mac.getInstance("HmacSHA1");
+    mac.init(signingKey);
+
+    // Compute the hmac on input data bytes
+    var rawHmac = mac.doFinal(new java.lang.String(value).getBytes());
+
+    // Convert raw bytes to Hex
+    var hexBytes = new org.apache.commons.codec.binary.Hex().encode(rawHmac);
+
+    //  Covert array of Hex bytes to a String
+    return new java.lang.String(hexBytes, "UTF-8");
+
 }
