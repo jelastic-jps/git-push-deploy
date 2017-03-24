6e5914bc10d12ab25d3fc10cf189341f8cb9674e
dzotic9 <dzotic9@gmail.com>
2017-03-24 16:19:12 +0200

    Create show-message.cs

---
+++ scripts/show-message.cs
@@ -0,0 +1,17 @@
+//@reg(type, javaLang, otherLangs)
+
+var successText = "success";
+
+if (type == "java") {
+  return {
+    type: successText,
+    message: javaLang,
+    email: javaLang"
+  }
+}
+
+return {
+    type: successText,
+    message: otherLangs,
+    email: otherLangs"
+};
