a412629d4ba82c3c33232d57753f89d38d7de878
sych74 <sych74@gmail.com>
2017-03-24 17:04:22 +0100

    Update add-vcs-project.cs

--- scripts/add-vcs-project.cs
+++ scripts/add-vcs-project.cs
@@ -19,6 +19,9 @@ var params = {
 resp = jelastic.env.vcs.DeleteProject(params.envName, params.session, params.context);
 if (resp.result != 0 && resp.result != 2500) return resp;
 
+resp = jelastic.env.control.RemoveApp(params.envName, params.session, params.context);
+if (resp.result != 0 && resp.result != 2313) return resp;
+
 //create and update the project 
 resp = jelastic.env.vcs.CreateProject(params.envName, params.session, params.type, params.context, params.url, params.branch, params.keyId, params.login, params.password, params.autoupdate, params.interval, params.autoResolveConflict, params.zdt);
 if (resp.result != 0) return resp;
