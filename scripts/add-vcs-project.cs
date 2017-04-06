885e2d521c2e1b59194baef1ccfdabafc7dd7172
sych74 <sych74@gmail.com>
2017-04-06 11:08:03 +0200

    removing all contexts for ruby

--- scripts/add-vcs-project.cs
+++ scripts/add-vcs-project.cs
@@ -1,30 +1,37 @@
 //@req(user, token, url, branch, type)
 
-var params = {
-   session: session,
-   envName: "${env.envName}",
-   type: "git",
-   url: url,
-   branch: branch,
-   login: user,
-   password: token,
-   keyId: null,
-   context: type == "ruby" ? "development" : "ROOT",
-   autoupdate: true,
-   interval: 1,
-   autoResolveConflict: true,
-   zdt: true
+context = type == "ruby" ? ["development", "test", "production"] : ["ROOT"];
+    
+p = {
+    session: session,
+    envName: "${env.envName}",
+    type: "git",
+    url: url,
+    branch: branch,
+    login: user,
+    password: token,
+    keyId: null,
+    context: context[0],
+    autoupdate: true,
+    interval: 1,
+    autoResolveConflict: true,
+    zdt: true
+}
+   
+//removing already deployed app/project in context
+//specifically for ruby -> removing all contexts (development, test and production), 
+//because ruby supports only one context at a time  
+for (i = 0; i < context.length; i++) {
+    resp = jelastic.env.control.RemoveApp(p.envName, p.session, context[i]);
+    if (resp.result != 0 && resp.result != 2313) return resp;
+
+    resp = jelastic.env.vcs.DeleteProject(p.envName, p.session, context[i]);
+    if (resp.result != 0 && resp.result != 2500) return resp;
 }
 
-resp = jelastic.env.control.RemoveApp(params.envName, params.session, params.context);
-if (resp.result != 0 && resp.result != 2313) return resp;
-
-resp = jelastic.env.vcs.DeleteProject(params.envName, params.session, params.context);
-if (resp.result != 0 && resp.result != 2500) return resp;
-
 //create and update the project 
-resp = jelastic.env.vcs.CreateProject(params.envName, params.session, params.type, params.context, params.url, params.branch, params.keyId, params.login, params.password, params.autoupdate, params.interval, params.autoResolveConflict, params.zdt);
+resp = jelastic.env.vcs.CreateProject(p.envName, p.session, p.type, p.context, p.url, p.branch, p.keyId, p.login, p.password, p.autoupdate, p.interval, p.autoResolveConflict, p.zdt);
 if (resp.result != 0) return resp;
 
-resp = jelastic.env.vcs.Update(params.envName, params.session, params.context);
+resp = jelastic.env.vcs.Update(p.envName, p.session, p.context);
 return resp;
