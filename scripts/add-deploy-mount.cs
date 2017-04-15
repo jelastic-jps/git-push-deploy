c278e50eedf3f706182b95beeb471c19f15a647c
sych74 <sych74@gmail.com>
2017-04-16 00:06:06 +0200

    Update add-deploy-mount.cs

--- scripts/add-deploy-mount.cs
+++ scripts/add-deploy-mount.cs
@@ -1,20 +1,20 @@
 //@auth 
-//@req(pathFrom, pathTo)
+//@req(pathFrom)
 
 var mountFrom = "${nodes.build.first.id}";
 var envName = "${settings.targetEnv}".split(".")[0];
 var mountTo = "cp";
-//var groups = jelastic.env.control.GetEnvInfo(envName, session).nodeGroups; 
-/*for (var i = 0; i < groups.length; i++){
-  if (groups[i].name == "storage") {
-    mountTo = "storage";
-    break;
-  }
-}*/
 
 var resp = jelastic.env.file.RemoveMountPointByGroup(envName, session, mountTo, pathTo);
 if (resp.result != 0) return resp;
 
-//resp = jelastic.env.control.AddDockerVolumeByGroup('${env.envName}', session, mountTo, volume); 
-resp = jelastic.env.file.AddMountPointByGroup(envName, session, mountTo, pathTo, 'nfs', null, pathFrom, mountFrom, 'auto-deploy-folder', false); 
+resp = jelastic.env.control.ExecCmdById(envName, session, "${nodes.build.cp.id}", toJSON([{
+       "command": "echo ${WEBROOT:-$Webroot_Path}"
+   }]) + "", true, "root");
+if (resp.result != 0) return resp;
+
+webroot = resp.responses[0].out;
+if (!webroot) return {result: 99, type: "error", message: "$WEBROOT is not found"}
+
+resp = jelastic.env.file.AddMountPointByGroup(envName, session, mountTo, webroot, 'nfs', null, pathFrom, mountFrom, 'auto-deploy-folder', false); 
 return resp;
