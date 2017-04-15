5dad48d7bf169ba8efb2e61dfe25334e65ebd374
sych74 <sych74@gmail.com>
2017-04-16 01:29:39 +0200

    Update add-deploy-mount.cs

--- scripts/add-deploy-mount.cs
+++ scripts/add-deploy-mount.cs
@@ -1,21 +1,29 @@
 //@auth 
-//@req(pathFrom, nodeId, nodeGroup)
+//@req(pathFrom, nodeGroup)
 
 var mountFrom = "${nodes.build.first.id}";
 var envName = "${settings.targetEnv}".split(".")[0];
 var mountTo = nodeGroup;
 
-//getting app deployment directory 
-resp = jelastic.env.control.ExecCmdById(envName, session, nodeId, toJSON([{
-       "command": "echo ${WEBROOT:-$Webroot_Path}"
-   }]) + "", true, "root");
+var autoDeployFolder;
+var nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes;
+for (var i = 0, l = nodes.length; i < l; i++) {
+       if (nodes[i].nodeGroup == nodeGroup) {
+              var type = nodes[i].nodeType;
+              if (type.indexOf("tomcat") > -1) {
+                     autoDeployFolder = "/opt/tomcat/webapps";
+              } else if (type.indexOf("glassfish") > -1) {
+                     autoDeployFolder="/opt/repo/versions/"+nodes[i].version+"/glassfish/domains/domain1/autodeploy";
+              }
+              break;
+       }
+}
+
+if (!autoDeployFolder) {
+   return {result: 99, type: "error", message: "$WEBROOT is not found"}
+}
+var resp = jelastic.env.file.RemoveMountPointByGroup(envName, session, mountTo, autoDeployFolder);
 if (resp.result != 0) return resp;
 
-webroot = resp.responses[0].out;
-if (!webroot) return {result: 99, type: "error", message: "$WEBROOT is not found"}
-
-var resp = jelastic.env.file.RemoveMountPointByGroup(envName, session, mountTo, webroot);
-if (resp.result != 0) return resp;
-
-resp = jelastic.env.file.AddMountPointByGroup(envName, session, mountTo, webroot, 'nfs', null, pathFrom, mountFrom, 'auto-deploy-folder', false); 
+resp = jelastic.env.file.AddMountPointByGroup(envName, session, mountTo, autoDeployFolder, 'nfs', null, pathFrom, mountFrom, 'auto-deploy-folder', false); 
 return resp;
