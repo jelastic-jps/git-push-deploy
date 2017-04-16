c7ef8be3db6608e43c93893a5dc4463267493cfe
sych74 <sych74@gmail.com>
2017-04-16 02:13:01 +0200

    Update add-deploy-mount.cs

--- scripts/add-deploy-mount.cs
+++ scripts/add-deploy-mount.cs
@@ -6,24 +6,26 @@ var envName = "${settings.targetEnv}".split(".")[0];
 var mountTo = nodeGroup;
 
 //TODO:move autoDeployFolder deterination logic to jem
-var autoDeployFolder;
+var autoDeployFolder, type;
 var nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes;
 for (var i = 0, l = nodes.length; i < l; i++) {
        if (nodes[i].nodeGroup == nodeGroup) {
-              var type = nodes[i].nodeType;
+              type = nodes[i].nodeType;
               if (type.indexOf("tomcat") > -1) {
                      autoDeployFolder = "/opt/tomcat/webapps";
               } else if (type == "glassfish4") {
                      autoDeployFolder="/opt/repo/versions/"+nodes[i].version+"/glassfish/domains/domain1/autodeploy";
-              }  if (type == "glassfish3") {
+              } if (type == "glassfish3") {
                      autoDeployFolder="/opt/"+type+"/glassfish/domains/domain1/autodeploy";
+              } if (type == "wildfly10") {
+                     autoDeployFolder="/opt/repo/versions/"+nodes[i].version+"/standalone/deployments";
               }
               break;
        }
 }
 
 if (!autoDeployFolder) {
-   return {result: 99, type: "error", message: "$WEBROOT is not found"}
+   return {result: 99, type: "error", message: "autodeploy folder is not defined for nodeType=" + type}
 }
 //---
 
