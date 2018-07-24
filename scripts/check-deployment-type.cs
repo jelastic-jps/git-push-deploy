558eb2b12faee9c5c14a6ec48081ea5eab6e73b6
Ruslan Synytsky <siruslan@gmail.com>
2018-07-24 21:44:42 +0200

    envName depends on addon type

--- scripts/check-deployment-type.cs
+++ scripts/check-deployment-type.cs
@@ -2,15 +2,18 @@
 
 nodes = jelastic.env.control.GetEnvInfo('${env.envName}', session).nodes
 addon = 'unknown'
+envName = '${env.envName}'
+
 for (i = 0; i < nodes.length; i++){
   if (nodes[i].nodeGroup == 'cp') {
     type = nodes[i].engineType || (nodes[i].activeEngine || {}).type;
     addon = type ? (type == 'java' ? 'maven' : 'vcs') : 'mount'
       
     if (addon == 'mount') return {result:99, error: 'deploy to custom containers is not implemented yet', type: 'warning'}
+    if (addon == 'maven') envName += '-git-push-${fn.random(1000)}'
     
     resp = {result: 0, onAfterReturn: []}
-    o = {}, o[next] = {addon: addon, type: type}
+    o = {}, o[next] = {addon: addon, type: type, envName: envName}
     resp.onAfterReturn.push(o)
 
     return resp
